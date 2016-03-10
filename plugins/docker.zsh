ea7caffffa53967cb4617e14c64c5e0aa533e087
Nelson Silva <nelson.silva@inevo.pt>
2016-03-10 10:45:36 +0000

    Add docker-up and docker-cleanup

--- plugins/docker.zsh
+++ plugins/docker.zsh
@@ -1,3 +1,12 @@
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+docker-up() {
+  docker-machine start $1 
+  eval "$(docker-machine env $1)"
+  echo "$1 machine ready at `docker-machine ip $1`"
+}
+
+docker-cleanup() {
+  echo "Removing dead and exited containers"
+  docker rm -v $(docker ps -a -q -f status=exited -f status=dead)
+  echo "Removing dangling images"
+  docker rmi $(docker images -q -f dangling=true)  
+}
